dca54dca5977b71465cb93bd6874b48da7916f1b
Marie Røgler <mroegler@gmail.com>
2020-09-26 08:58:20 +0200

    adding a dumb component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="primary-btn" v-text="title"></button>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String
+    }
+};
+</script>
+
+<style>
+.primary-btn {
+    background: blue;
+    color: cornsilk;
+    border: none;
+}
+
+
+</style>
+    
--- src/components/RecepieItem.vue
+++ src/components/RecepieItem.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
         <h1>{{ item.title }}</h1>
+        <ButtonComponent :title="'Read more'"/>
         <p><b>Ingredients:</b> {{ item.ingredients }}</p>
         <img :src="item.thumbnail" height="300"/>
     </div>
 </template>
 
 <script>
+import ButtonComponent from "./ButtonComponent.vue"
+
 export default {
-    name: 'RecepieItem',
+    name: 'RecepieItem', 
+    components: {
+        ButtonComponent
+    },
     props: {
        item: Object 
     }
